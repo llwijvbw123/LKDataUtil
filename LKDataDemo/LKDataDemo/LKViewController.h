bea22f7d8f3ee4c3f33560e44d84c17910062438
licrk <licrk@test.com>
2013-12-12 10:23:25 +0800

    add project

---
+++ LKDataDemo/LKDataDemo/LKViewController.h
@@ -0,0 +1,13 @@
+//
+//  LKViewController.h
+//  LKDataDemo
+//
+//  Created by licrk on 13-12-12.
+//  Copyright (c) 2013年 LK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LKViewController : UIViewController
+
+@end
---
+++ LKDataDemo/LKDataDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LKDataDemo
+//
+//  Created by licrk on 13-12-12.
+//  Copyright (c) 2013年 LK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LKAppDelegate class]));
+    }
+}
